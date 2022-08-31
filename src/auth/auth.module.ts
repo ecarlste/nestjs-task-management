503fbee66b6f4468dad479f402215f014a44d0b2
Erik S. Carlsten <ecarlste@gmail.com>
2022-08-31 11:53:58 -0600

    Added jwt validation strategy and test endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,17 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'topSecret51',
       signOptions: { expiresIn: '3600s' },
     }),
     TypeOrmModule.forFeature([User]),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
